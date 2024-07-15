34fc8c38977768079d4834277a7bf97614531eb9
Jacek Tomaszewski <jacek@fullstack.house>
2024-07-15 15:15:48 +0200

    add a script for building the package

---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import { registerApiMutationProvider } from "./code-components/ApiProvider/ApiMutationProvider.register";
+import { registerApiProvider } from "./code-components/ApiProvider/ApiProvider.register";
+import { registerAuthContext } from "./code-components/AuthContext/AuthContext.register";
+import { registerOnBeforeLogoutProvider } from "./code-components/AuthContext/LogoutProvider/OnBeforeLogoutProvider.register";
+import { registerCombobox } from "./code-components/Combobox/Combobox.register";
+import { registerDebounceProvider } from "./code-components/DebounceProvider/DebounceProvider.register";
+import { registerDialogV2 } from "./code-components/DialogV2/DialogV2.register";
+import { registerFollowingTooltip } from "./code-components/FollowingTooltip/FollowingTooltip.register";
+import { registerFormProvider } from "./code-components/form/FormProvider.register";
+import { registerSwitch } from "./code-components/form/Switch/Switch.register";
+import { registerHiddenIfEmptyContainer } from "./code-components/HiddenIfEmptyContainer/HiddenIfEmptyContainer.register";
+import { registerOnBeforeUnloadProvider } from "./code-components/OnBeforeUnloadProvider/OnBeforeUnloadProvider.register";
+import { registerOnBeforeUnmountProvider } from "./code-components/OnBeforeUnmountProvider/OnBeforeUnmountProvider.register";
+import { registerOnChangeProvider } from "./code-components/OnChangeProvider/OnChangeProvider.register";
+import { registerRawCheckboxOrRadio } from "./code-components/RawCheckboxOrRadio/CheckboxOrRadio.register";
+import { registerRawChildren } from "./code-components/RawChildren/Children.register";
+import { registerRawList } from "./code-components/RawList/RawList.register";
+import { registerRawNull } from "./code-components/RawNull/Null.register";
+import { registerRawTable } from "./code-components/RawTable/RawTable.register";
+import { registerRouteQuerySynchronizer } from "./code-components/Router/RouteQuerySynchronizer.register";
+import { registerRouter } from "./code-components/Router/Router.register";
+import { registerStateProvider } from "./code-components/StateProvider/StateProvider.register";
+import { registerTextLinkOrButton } from "./code-components/TextLinkOrButton/TextLinkOrButton.register";
+import { registerTimeoutProvider } from "./code-components/TimeoutProvider/TimeoutProvider.register";
+import { registerWindowEventListener } from "./code-components/WindowEventListener/WindowEventListener.register";
+import { registerDayjs } from "./custom-functions/dayjs.register";
+import { registerParseDateWithoutTimeZone } from "./custom-functions/parseDateWithoutTimeZone.register";
+import { registerWaitFor } from "./custom-functions/waitFor.register";
+import { registerZod } from "./custom-functions/zod.register";
+import { registerMyEvaluationsPlasmicUtils } from "./register";
+
+export {
+  registerApiMutationProvider,
+  registerApiProvider,
+  registerAuthContext,
+  registerCombobox,
+  registerDayjs,
+  registerDebounceProvider,
+  registerDialogV2,
+  registerFollowingTooltip,
+  registerFormProvider,
+  registerHiddenIfEmptyContainer,
+  registerMyEvaluationsPlasmicUtils,
+  registerOnBeforeLogoutProvider,
+  registerOnBeforeUnloadProvider,
+  registerOnBeforeUnmountProvider,
+  registerOnChangeProvider,
+  registerParseDateWithoutTimeZone,
+  registerRawCheckboxOrRadio,
+  registerRawChildren,
+  registerRawList,
+  registerRawNull,
+  registerRawTable,
+  registerRouteQuerySynchronizer,
+  registerRouter,
+  registerStateProvider,
+  registerSwitch,
+  registerTextLinkOrButton,
+  registerTimeoutProvider,
+  registerWaitFor,
+  registerWindowEventListener,
+  registerZod,
+};
