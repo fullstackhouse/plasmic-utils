4c632acbd143bc5729f84f096c964036af6dfa3b
Jacek Tomaszewski <jacek@fullstack.house>
2025-02-05 14:26:30 +0100

    feat: add `MemoDataProvider` code component (#21)
    * feat: add `MemoDataProvider` code component
    
    * fix(MemoDataProvider): avoid creating new empty object on every render

---
+++ src/code-components/MemoDataProvider/MemoDataProvider.tsx
@@ -0,0 +1,49 @@
+import {
+  DataContext,
+  DataProviderProps,
+  mkMetaName,
+  mkMetaValue,
+  useDataEnv,
+} from "@plasmicapp/react-web/lib/host";
+import { useMemo } from "react";
+
+interface MemoDataProviderProps extends DataProviderProps {
+  memoKey?: unknown | unknown[];
+}
+
+export function MemoDataProvider<T>({
+  name,
+  data,
+  memoKey,
+  hidden,
+  advanced,
+  label,
+  children,
+}: MemoDataProviderProps) {
+  const parentContext = useDataEnv();
+  const actualMemoKeys =
+    memoKey == null
+      ? [JSON.stringify(data)]
+      : Array.isArray(memoKey)
+        ? memoKey
+        : [memoKey];
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const memoizedData = useMemo(() => data, actualMemoKeys);
+
+  const childContext = useMemo(() => {
+    if (!name) {
+      return parentContext;
+    }
+    return {
+      ...parentContext,
+      [name]: memoizedData,
+      [mkMetaName(name)]: mkMetaValue({ hidden, advanced, label }),
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [parentContext, name, memoizedData]);
+
+  return (
+    <DataContext.Provider value={childContext}>{children}</DataContext.Provider>
+  );
+}
