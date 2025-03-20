11a0ba69f58278cf8ab9e42822f0977d70ef74c8
Jacek Tomaszewski <jacek@fullstack.house>
2025-03-20 09:34:44 +0100

    fix(Router): remove `useLayoutEffect` usage warning on SSR

--- src/code-components/Router/useRoute.tsx
+++ src/code-components/Router/useRoute.tsx
@@ -1,10 +1,11 @@
-import { useState, useLayoutEffect } from "react";
-import { RouteStorage, Route } from "./storage/base";
+import { useState } from "react";
+import { useIsomorphicLayoutEffect } from "../../common/useIsomorphicLayoutEffect";
+import { Route, RouteStorage } from "./storage/base";
 
 export function useRoute(storage: RouteStorage): Route {
   const [route, setRoute] = useState(storage.getRoute());
 
-  useLayoutEffect(() => {
+  useIsomorphicLayoutEffect(() => {
     storage.onRouteChange(setRoute);
 
     return () => {
