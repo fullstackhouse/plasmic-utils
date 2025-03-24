8fda3c0b0f795dff2cab23c1620bd1b633fbb52f
Jacek Tomaszewski <jacek@fullstack.house>
2025-03-24 11:46:24 +0100

    refactor(api): decompose json middleware internal code

--- src/code-components/ApiProvider/middlewares/json.ts
+++ src/code-components/ApiProvider/middlewares/json.ts
@@ -1,44 +1,60 @@
 import { FetchError } from "../FetchError";
-import { ApiMiddleware } from "./middleware";
+import { ApiMiddleware, ApiRequest } from "./middleware";
+
+class ApiError extends Error {}
 
 export const jsonApiMiddleware: ApiMiddleware = {
   name: "json",
   async fetch(request) {
-    const url = addQueryToPath(request.path, request.query);
-
-    let response: Response;
+    let response: Response | null = null;
     try {
-      response = await fetch(url, {
-        headers: {
-          ...(request.body
-            ? { "Content-Type": "application/json" }
-            : undefined),
-          ...request.headers,
-        },
-        method: request.method,
-        body: request.body ? JSON.stringify(request.body) : undefined,
-      });
-    } catch (error) {
-      throw new FetchError(url, null, error);
-    }
+      response = await fetchResponse(request);
 
-    if (response.ok) {
-      try {
-        return await response.json();
-      } catch (error) {
-        throw new FetchError(
-          url,
-          response,
-          new TypeError(`response returned unparseable JSON`, {
-            cause: error,
-          }),
+      if (!response.ok) {
+        throw new ApiError(
+          `response returned unsucccessful code (${response.status})`,
         );
       }
-    }
 
-    throw new FetchError(url, response);
+      return await parseResponseBody(response);
+    } catch (error) {
+      if (error instanceof ApiError) {
+        throw new FetchError(request.path, response, error);
+      }
+
+      throw error;
+    }
   },
 };
+
+async function fetchResponse(request: ApiRequest): Promise<Response> {
+  const url = addQueryToPath(request.path, request.query);
+
+  try {
+    return await fetch(url, {
+      headers: {
+        ...(request.body ? { "Content-Type": "application/json" } : undefined),
+        ...request.headers,
+      },
+      method: request.method,
+      body: request.body ? JSON.stringify(request.body) : undefined,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new ApiError(error.message, { cause: error });
+    }
+
+    throw new ApiError("fetch failed", { cause: error });
+  }
+}
+
+async function parseResponseBody(response: Response): Promise<unknown> {
+  try {
+    return await response.json();
+  } catch (error) {
+    throw new ApiError(`response returned unparseable JSON`);
+  }
+}
 
 function addQueryToPath(path: string, query?: Record<string, string>): string {
   return query
