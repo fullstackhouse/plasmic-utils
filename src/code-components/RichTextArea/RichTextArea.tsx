ce90b5032e3956bdc45be40cc4f6d425229a8d2b
dosmiko7 <mikolaj.oberda@gmail.com>
2025-03-26 07:17:50 +0100

    feat: add example from quill playground

---
+++ src/code-components/RichTextArea/RichTextArea.tsx
@@ -0,0 +1,60 @@
+// Example from: https://quilljs.com/playground/react
+
+import React, { useRef, useState } from 'react';
+import Editor from './Editor';
+
+const Delta = Quill.import('delta');
+
+export function RichTextArea() {
+  const [range, setRange] = useState();
+  const [lastChange, setLastChange] = useState();
+  const [readOnly, setReadOnly] = useState(false);
+
+  const quillRef = useRef();
+
+  return (
+    <div>
+      <Editor
+        ref={quillRef}
+        readOnly={readOnly}
+        defaultValue={new Delta()
+          .insert('Hello')
+          .insert('\n', { header: 1 })
+          .insert('Some ')
+          .insert('initial', { bold: true })
+          .insert(' ')
+          .insert('content', { underline: true })
+          .insert('\n')}
+        onSelectionChange={setRange}
+        onTextChange={setLastChange}
+      />
+      <div class="controls">
+        <label>
+          Read Only:{' '}
+          <input
+            type="checkbox"
+            value={readOnly}
+            onChange={(e) => setReadOnly(e.target.checked)}
+          />
+        </label>
+        <button
+          className="controls-right"
+          type="button"
+          onClick={() => {
+            alert(quillRef.current?.getLength());
+          }}
+        >
+          Get Content Length
+        </button>
+      </div>
+      <div className="state">
+        <div className="state-title">Current Range:</div>
+        {range ? JSON.stringify(range) : 'Empty'}
+      </div>
+      <div className="state">
+        <div className="state-title">Last Change:</div>
+        {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
+      </div>
+    </div>
+  );
+};
