cabdcf63b4b62716f52328d5dfd4dcff3ca79eac
dosmiko7 <mikolaj.oberda@gmail.com>
2025-03-26 12:39:54 +0100

    chore: remove temporary helpers

--- src/code-components/RichTextArea/RichTextArea.tsx
+++ src/code-components/RichTextArea/RichTextArea.tsx
@@ -22,9 +22,6 @@ export function RichTextArea({
   placeholder,
   readOnly,
 }: RichTextAreaProps) {
-  const [range, setRange] = useState<Range | null>(null);
-  const [lastChange, setLastChange] = useState<string>();
-
   const quillRef = useRef();
 
   const formattedToolbar = useMemo(() => formatDefaultToolbarConfigs(toolbar), [JSON.stringify(toolbar)]);
@@ -47,25 +44,6 @@ export function RichTextArea({
           onChange?.(content, source);
         }}
       />
-      <div>
-        <button
-          className="controls-right"
-          type="button"
-          onClick={() => {
-            alert(quillRef.current?.getLength());
-          }}
-        >
-          Get Content Length
-        </button>
-      </div>
-      <div className="state">
-        <div className="state-title">Current Range:</div>
-        {range ? JSON.stringify(range) : 'Empty'}
-      </div>
-      <div className="state">
-        <div className="state-title">Last Change:</div>
-        {lastChange ? JSON.stringify(lastChange.ops) : 'Empty'}
-      </div>
     </div>
   );
 };
