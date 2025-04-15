105287354c52e79136517ab9429cb0de6e95d85f
Jacek Tomaszewski <jacek@fullstack.house>
2025-04-15 14:03:41 +0200

    fix(RichTextArea): set bounds for quill editor

--- src/code-components/RichTextArea/RichTextArea.tsx
+++ src/code-components/RichTextArea/RichTextArea.tsx
@@ -1,6 +1,6 @@
 import type { ImageData } from "quill-image-drop-and-paste";
 import "quill/dist/quill.snow.css";
-import { CSSProperties, useMemo, useRef } from "react";
+import { CSSProperties, useId, useMemo, useRef } from "react";
 import type ReactQuillNew from "react-quill-new";
 import styles from "./quill.module.css";
 import { QuillPlaceholder } from "./QuillPlaceholder";
@@ -61,24 +61,30 @@ export function RichTextArea(
   }, [actualToolbar, imageHandler]);
 
   const actualClassName = `${className} ${styles.container} ${imageUploading ? styles.containerImageUploading : ""}`;
+  const id = useId();
 
   if (!pkgs) {
     return (
-      <QuillPlaceholder
-        {...reactQuillProps}
-        className={actualClassName}
-        style={style}
-      />
+      <div data-rich-text-area-container={id}>
+        <QuillPlaceholder
+          {...reactQuillProps}
+          className={actualClassName}
+          style={style}
+        />
+      </div>
     );
   }
 
   return (
-    <pkgs.ReactQuill.default
-      {...reactQuillProps}
-      ref={quillRef}
-      className={actualClassName}
-      style={style}
-      modules={modules}
-    />
+    <div data-rich-text-area-container={id}>
+      <pkgs.ReactQuill.default
+        {...reactQuillProps}
+        ref={quillRef}
+        className={actualClassName}
+        style={style}
+        modules={modules}
+        bounds={`[data-rich-text-area-container="${id}"]`}
+      />
+    </div>
   );
 }
